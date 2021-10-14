176dad2288cf038ef5be98379f72fb73e5e38461
Jairo Alzate <jairo.alzate@siesa.com>
2021-10-14 15:12:04 -0500

    Add posts to tab1

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,18 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from 'src/app/interfaces/interfaces';
 import { PostsService } from '../../services/posts.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
-export class Tab1Page implements OnInit{
+export class Tab1Page implements OnInit {
+  posts: Post[] = [];
 
   constructor(private postsService: PostsService) {}
 
   ngOnInit() {
-    this.postsService.getPosts().subscribe( resp => {
-      console.log(resp);
+    this.postsService.getPosts().subscribe((resp) => {
+      this.posts.push(...resp.posts);
     });
   }
 }
